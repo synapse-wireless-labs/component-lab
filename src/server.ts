341a0bcfb3816fa65c3705f2386757a447d8ef4f
Jed <jorrdandied@hotmail.com>
2017-03-15 17:55:15 +0000

    feat(server): Support custom devServer options (#14)
    My project needed some components running under https, so had to do some tweaking of the config setup
    
    * tidy up server config

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,33 @@ const ProgressPlugin = require('webpack/lib/ProgressPlugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+export interface DevServerConfig {
+  port?: number;
+  proxy?: any;
+  host?: string;
+  quiet?: boolean;
+  noInfo?: boolean;
+  watchOptions?: any;
+  https?: boolean | {key: any; cert: any; };
+}
+
 
 export function startServer(config: ComponentLabConfig, suite: string) {
-  const webpackConfig = config.webpackConfig;
+  const webpackConfig: any = config.webpackConfig;
+  const devServerConfig: DevServerConfig = webpackConfig.devServer;
+  let https = false,
+    host = config.host || "localhost",
+    port = config.port || 8080;
+  if (devServerConfig) {
+    https = !!devServerConfig.https;
+    host = devServerConfig.host || host;
+    port = devServerConfig.port || port;
+  }
   const includes = config.include || [];
 
   webpackConfig.entry = {
     main: [
-      `webpack-dev-server/client?http://${config.host}:${config.port}/`,
+      `webpack-dev-server/client?http${https?'s':''}://${host}:${port}/`,
       ...includes,
       config.suites[suite]
     ]
@@ -32,7 +51,7 @@ export function startServer(config: ComponentLabConfig, suite: string) {
     template: path.resolve(__dirname, '../index.html')
   }));
 
-  const serverConfig = {
+  const serverConfig = Object.assign({}, {
     historyApiFallback: true,
     stats: {
       assets: true,
@@ -44,12 +63,12 @@ export function startServer(config: ComponentLabConfig, suite: string) {
       chunkModules: false  
     },
     inline: true
-  };
+  }, devServerConfig);
 
   const server = new WebpackDevServer(compiler, serverConfig);
 
   return new Promise((resolve, reject) => {
-    server.listen(config.port, `${config.host}`, function(err, stats) {
+    server.listen(port, `${host}`, function(err, stats) {
       if (err) {
         console.error(err.stack || err);
         if (err.details) { console.error(err.details); }
