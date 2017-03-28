28c2a0d1ec109c3b27d0c9ce6c9fba0872f001cc
Dominic Watson <intellix@users.noreply.github.com>
2017-03-28 22:20:35 +0200

    chore(): Remove lodash dependency (#16)

--- src/frontend/module-builder.ts
+++ src/frontend/module-builder.ts
@@ -1,6 +1,5 @@
 import { NgModule, Component, Type, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { flatten } from 'lodash';
 import { Experiment, ExperimentCase } from './models/experiment';
 import { ResolvedLab } from './models/lab';
 
@@ -9,7 +8,7 @@ export function getModuleForExperiments(
   inputModule: ModuleWithProviders | Type<any>,
   experiments: Experiment[]
 ): ResolvedLab {
-  const componentsWithIds = flatten(experiments.map(exp => exp.cases.map(c => {
+  const componentsWithIds = [].concat(...experiments.map(exp => exp.cases.map(c => {
     return {
       id: c.id,
       component: generateComponent(c)
